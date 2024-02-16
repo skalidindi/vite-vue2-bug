ccfb352cd8cf68953d87d4f11d9c493e28baf898
Santosh Kalidindi <skalidindi8@gmail.com>
2024-02-15 23:03:18 -0800

    initial version for reproduction

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main :class="$style.main">
+    Hello World! Check network for fetching the css file for App
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+<style module>
+:global(.some-class) {
+  color: red;
+}
+
+.main {
+  background-color: #f0f0f0;
+}
+</style>
